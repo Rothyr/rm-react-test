f0008a0c89ba61d4709d9b9a08b7511660827e3f
Dan Amiri-Fear <danielowenfear@gmail.com>
2019-06-19 21:40:26 -0700

    pushes class additions

--- src/Components/Navbar/SelectDropdownValue.jsx
+++ src/Components/Navbar/SelectDropdownValue.jsx
@@ -6,7 +6,7 @@ class SelectDropdownValue extends Component {
   render() {
     return (
       <div>
-        <select onChange={this.props.changeTheSelection}>
+        <select onChange={this.props.changeTheSelection} id="host" className="btn btn-secondary btn-lg btn-satel" name="host">
           <option value='repo'> Github </option>
           <option value='project'> Jira </option>
         </select>
